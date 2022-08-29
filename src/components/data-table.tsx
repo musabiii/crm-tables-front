131ee57712ebde8b010bd86e7712db1ca539e8e7
Musabii Mambetov <musabi@live.com>
2022-08-29 22:52:20 +0300

    migrate logic to data-table from parent

--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -1,64 +1,196 @@
-import React, { useState } from "react";
-import { EOrder, IColumn } from "../models/models";
+import React, { useEffect, useState } from "react";
+import { EOrder, IClient, IColumn } from "../models/models";
+import { Filter } from "./filter";
+import { Pagination } from "./pagination";
 
 interface IDataTable {
-  data: any[];
   columns: IColumn[];
-  sortCol: string;
-  order: EOrder;
-  setSortCol: Function;
-  setOrder: Function;
+  lazyFetch: Function;
+  fetchData:Function
+  data:any[]
 }
 
-export default function DataTable({ data, columns,sortCol,order,setSortCol,setOrder}: IDataTable) {
+export default function DataTable({ columns, fetchData,data }: IDataTable) {
 
-  const [selectedRow,setSelectedRow] = useState(0)
+  const [selectedRow, setSelectedRow] = useState(0);
+  const [sortCol, setSortCol] = useState("id");
+  const [order, setOrder] = useState<EOrder>(EOrder.asc);
+  const [filterCol, setFilterCol] = useState("id");
+  const [filterCompare, setFilterCompare] = useState("=");
+  const [filterValue, setFilterValue] = useState("");
+  const [page, setPage] = useState<number>(1);
+  const [infinityScroll, setInfinityScroll] = useState<boolean>(false);
+  const [listened, setListened] = useState(false);
+
+  const fetchOptions = () => {
+    return {
+      sortCol,
+      order,
+      filterCol,
+      filterCompare,
+      filterValue,
+      page,
+    };
+  };
+
+  // const [fetchData, { data }] = lazyFetch();
+
+  const [list, setList] = useState(data);
+
+  //* update fetch
+  useEffect(() => {
+    console.log("use effect [fetch]Data(fetchOptions()");
+    fetchData(fetchOptions());
+  }, [sortCol, order, filterCol, filterCompare, filterValue]);
+
+  //* mount
+  useEffect(() => {
+    fetchData(fetchOptions());
+  }, []);
 
   // const [list,setList] = useState(data);
 
-  const sortType = (col:string) => {
+  const dt = document.querySelector(".data-table");
+
+  function listener() {
+    if (
+      (dt?.scrollTop || 0) + (dt?.clientHeight || 0) + 20 >=
+      (dt?.scrollHeight || 0)
+    ) {
+      console.log("scroll more", page + 1);
+      setPage(page + 1);
+    }
+  }
+
+  //*  [page]
+  useEffect(() => {
+    console.log("use effect [page]");
+    if (!infinityScroll) {
+      setList([]);
+      dt?.scrollTo(0, 0);
+    }
+    fetchData(fetchOptions());
+    console.log(dt);
+    if (dt && infinityScroll) {
+      setTimeout(() => {
+        console.log("set listener");
+        dt.addEventListener("scroll", listener);
+      }, 300);
+    }
+
+    return () => {
+      dt?.removeEventListener("scroll", listener);
+      console.log("destroy");
+    };
+  }, [page]);
+
+  //* [infinityScroll]
+  useEffect(() => {
+    if (dt && infinityScroll) {
+      dt.addEventListener("scroll", listener);
+    } else if (dt && !infinityScroll) {
+      console.log("remove listener");
+    }
+
+    return () => {
+      dt?.removeEventListener("scroll", listener);
+    };
+  }, [infinityScroll]);
+
+  const handleChangeInfinityScroll = () => {
+    if (page !== 1) {
+      if (infinityScroll) {
+        setList([]);
+      }
+      console.log("handleChangeInfinityScroll");
+      setPage(1);
+    }
+    dt?.scrollTo(0, 0);
+    setInfinityScroll(!infinityScroll);
+  };
+
+  useEffect(() => {
+    console.log("useEffect [data]", data);
+    if (infinityScroll) {
+      setList([...(list || []), ...(data || [])]);
+    } else {
+      setList(data);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    console.log("change list", list);
+  }, [list]);
+
+  const sortType = (col: string) => {
     if (sortCol === col) {
       return order;
     }
     return null;
-  }
+  };
 
-  const handleClickRow = (e:React.MouseEvent<HTMLTableRowElement>,id:number) => {
+  const handleClickRow = (
+    e: React.MouseEvent<HTMLTableRowElement>,
+    id: number
+  ) => {
     console.log(e.detail);
     if (e.detail === 2) {
-
     }
 
-    if (id===selectedRow) {
-      setSelectedRow(0)
+    if (id === selectedRow) {
+      setSelectedRow(0);
     } else {
-      setSelectedRow(id)
+      setSelectedRow(id);
     }
-  }
+  };
 
-  const handleSetSort = (col:string) => {
-    if (col===sortCol) {
-      setOrder(order === EOrder.asc?EOrder.desc:EOrder.asc)
+  const handleSetSort = (col: string) => {
+    if (col === sortCol) {
+      setOrder(order === EOrder.asc ? EOrder.desc : EOrder.asc);
     } else {
-      setOrder(EOrder.asc)
-      setSortCol(col)
+      setOrder(EOrder.asc);
+      setSortCol(col);
     }
-  }
+  };
 
   return (
-    <div className="data-table">
-      <table >
+    <div >
+      <Filter
+        columns={columns}
+        filterCol={filterCol}
+        filterCompare={filterCompare}
+        filterValue={filterValue}
+        setFilterCol={setFilterCol}
+        setFilterCompare={setFilterCompare}
+        setFilterValue={setFilterValue}
+      />
+
+      <div className="data-table">
+
+      <table>
         <thead>
           <tr>
             {columns.map(
-              (col) => col.visible && <td onClick={()=>handleSetSort(col.title)} key={col.title}>{col.title}<SortBlock sortType={sortType(col.title)} /></td>
+              (col) =>
+                col.visible && (
+                  <td onClick={() => handleSetSort(col.title)} key={col.title}>
+                    {col.title}
+                    <SortBlock sortType={sortType(col.title)} />
+                  </td>
+                )
             )}
           </tr>
         </thead>
         <tbody>
-          {data.map((row) => {
+          {list.map((row) => {
             return (
-              <tr key={row.id} className={`table-row ${selectedRow===row.id?"table-row-selected":""}`} onClick={(e)=>handleClickRow(e,row.id)}>
+              <tr
+                key={row.id}
+                className={`table-row ${
+                  selectedRow === row.id ? "table-row-selected" : ""
+                }`}
+                onClick={(e) => handleClickRow(e, row.id)}
+              >
                 {columns.map((col) => {
                   return (
                     col.visible && <td key={col.title}>{row[col.title]}</td>
@@ -69,14 +201,27 @@ export default function DataTable({ data, columns,sortCol,order,setSortCol,setOr
           })}
         </tbody>
       </table>
+      </div>
+
+      <Pagination
+        page={page}
+        setPage={setPage}
+        infinityScroll={infinityScroll}
+        handleChangeInfinityScroll={handleChangeInfinityScroll}
+      />
     </div>
   );
 }
 
-
-function SortBlock({sortType}:any) {
-
+function SortBlock({ sortType }: any) {
   return (
-  <span className="sort-block" ><span className={sortType===EOrder.asc?'arr-ordered':'arr'}>&#8595;</span><span className={sortType===EOrder.desc?'arr-ordered':'arr'}>&#8593;</span></span>
-  )
+    <span className="sort-block">
+      <span className={sortType === EOrder.asc ? "arr-ordered" : "arr"}>
+        &#8595;
+      </span>
+      <span className={sortType === EOrder.desc ? "arr-ordered" : "arr"}>
+        &#8593;
+      </span>
+    </span>
+  );
 }
--- src/pages/clients.tsx
+++ src/pages/clients.tsx
@@ -2,120 +2,12 @@ import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import { Actions } from "../components/actions";
 import DataTable from "../components/data-table";
-import { Filter } from "../components/filter";
-import { Pagination } from "../components/pagination";
-import { useGetClientsQuery, useLazyGetClientsQuery } from "../store/crm.api";
-import { EOrder, IClient, IColumn } from "../models/models";
-import { Sort } from "../components/sort";
+import { IColumn } from "../models/models";
+import { useLazyGetClientsQuery } from "../store/crm.api";
 
 export default function Clients() {
-  const [sortCol, setSortCol] = useState("id");
-  const [order, setOrder] = useState<EOrder>(EOrder.asc);
-  const [filterCol, setFilterCol] = useState("id");
-  const [filterCompare, setFilterCompare] = useState("=");
-  const [filterValue, setFilterValue] = useState("");
-  const [page, setPage] = useState<number>(1);
-  const [infinityScroll, setInfinityScroll] = useState<boolean>(false);
 
-  const [fetchClients, { data }] = useLazyGetClientsQuery();
 
-  const [listened, setListened] = useState(false);
-
-  const [list, setList] = useState(data);
-
-  const fetchOptions = () => {
-    return {
-      sortCol,
-      order,
-      filterCol,
-      filterCompare,
-      filterValue,
-      page,
-    };
-  };
-
-  //* update fetch
-  useEffect(() => {
-    console.log("use effect [fetch]");
-    fetchClients(fetchOptions());
-  }, [sortCol, order, filterCol, filterCompare, filterValue]);
-
-  //* mount
-  useEffect(() => {
-    fetchClients(fetchOptions());
-  }, []);
-
-  const dt = document.querySelector(".data-table");
-
-  function listener() {
-    if (
-      (dt?.scrollTop || 0) + (dt?.clientHeight || 0) + 20 >=
-      (dt?.scrollHeight || 0)
-    ) {
-      console.log("scroll more", page + 1);
-      setPage(page + 1);
-    }
-  }
-
-  //*  [page]
-  useEffect(() => {
-    console.log("use effect [page]");
-    if (!infinityScroll) {
-      setList([]);
-      dt?.scrollTo(0, 0);
-    }
-    fetchClients(fetchOptions());
-    console.log(dt);
-    if (dt && infinityScroll) {
-      setTimeout(() => {
-        console.log("set listener");
-        dt.addEventListener("scroll", listener);
-      }, 300);
-    }
-
-    return () => {
-      dt?.removeEventListener("scroll", listener);
-      console.log("destroy");
-    };
-  }, [page]);
-
-  //* [infinityScroll]
-  useEffect(() => {
-    if (dt && infinityScroll) {
-      dt.addEventListener("scroll", listener);
-    } else if (dt && !infinityScroll) {
-      console.log("remove listener");
-    }
-
-    return () => {
-      dt?.removeEventListener("scroll", listener);
-    };
-  }, [infinityScroll]);
-
-  const handleChangeInfinityScroll = () => {
-    if (page !== 1) {
-      if (infinityScroll) {
-        setList([]);
-      }
-      console.log("handleChangeInfinityScroll");
-      setPage(1);
-    }
-    dt?.scrollTo(0, 0);
-    setInfinityScroll(!infinityScroll);
-  };
-
-  useEffect(() => {
-    console.log("useEffect [data]", data);
-    if (infinityScroll) {
-      setList([...(list || []), ...(data || [])]);
-    } else {
-      setList(data);
-    }
-  }, [data]);
-
-  useEffect(() => {
-    console.log("change list", list);
-  }, [list]);
 
   const columnsList: IColumn[] = [
     {
@@ -154,35 +46,21 @@ export default function Clients() {
     });
   };
 
+  const [fetchData, { data }] = useLazyGetClientsQuery();
+
   return (
     <div className="client page">
       <Link to="/" className="home-link">
         Home
       </Link>
-      <Filter
-        columns={columns}
-        filterCol={filterCol}
-        filterCompare={filterCompare}
-        filterValue={filterValue}
-        setFilterCol={setFilterCol}
-        setFilterCompare={setFilterCompare}
-        setFilterValue={setFilterValue}
-      />
       <Actions columns={columns} changeVisible={changeVisible} />
       <DataTable
-        data={list ?? []}
         columns={columns}
-        order={order}
-        sortCol={sortCol}
-        setSortCol={setSortCol}
-        setOrder={setOrder}
+        lazyFetch = {useLazyGetClientsQuery}
+        fetchData = {fetchData}
+        data = {data??[]}
       />
-      <Pagination
-        page={page}
-        setPage={setPage}
-        infinityScroll={infinityScroll}
-        handleChangeInfinityScroll={handleChangeInfinityScroll}
-      />
+
     </div>
   );
 }
