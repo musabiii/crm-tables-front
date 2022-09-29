a7a6ff19d72ba24a9dfa452fc1c6060ea5174328
Musabii Mambetov <musabi@live.com>
2022-09-29 16:06:12 +0300

    api ip

--- src/store/crm.api.ts
+++ src/store/crm.api.ts
@@ -4,8 +4,8 @@ import { IClient, IDocument, IQuery, IService } from "../models/models";
 export const crmApi = createApi({
   reducerPath: "crm/api",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "http://89.108.98.131:3000/api/",
-    baseUrl: "http://localhost:4000/api/",
+    baseUrl: "http://213.226.126.113:3000/api/",
+    // baseUrl: "http://localhost:4000/api/",
     // mode:"no-cors"
   }),
   endpoints: (build) => ({
