8671016238e1ecb40e89a9ecc6cd07ed7cb739d5
Musabii Mambetov <musabi@live.com>
2022-10-01 22:07:31 +0300

    Revert "rm test"
    This reverts commit 9f355d4ee6c571b43f83506b415a65dad33c2a9d.

--- src/store/crm.api.ts
+++ src/store/crm.api.ts
@@ -5,6 +5,7 @@ export const crmApi = createApi({
   reducerPath: "crm/api",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://213.226.126.113:3000/api/",
+    // baseUrl: "http://localhost:4000/api/",
     // mode:"no-cors"
   }),
   endpoints: (build) => ({
